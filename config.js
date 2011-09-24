8a56ebefe348da9e53b915f9a9c8ecd9aa1b1627
Simon Holywell <simon@holywell.com.au>
2011-09-24 17:08:11 +0100

    Add in extra for config setup

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ exports.settings = {
     "status_message": "A demo Node.js Google Chat Bot",
     "client": {
         "jid": "n.js.bot@gmail.com",
-	"password": process.env.bot_password,
+	"password": "",
         "host": "talk.google.com",
         "port": 5222
     },
